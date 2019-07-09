3f88a1457e2e3083a0552ed3df0d5b14bcaa79f4
mohammad ehsan mohit <mohammadehsan.m@780.ir>
2019-07-09 18:59:53 +0430

    added bloc class

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,9 +1,51 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget{
-
+class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          Container(
+            margin: EdgeInsets.all(10.0),
+          ),
+          submitButton(),
+        ],
+      ),
+    );
+  }
 
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+          hintText: 'your@example.com',
+          labelText: 'Email Address',
+        errorText: 'invalid email address',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: '8-10 lenght',
+        labelText: 'Password'
+      ),
+    );
+  }
+
+  Widget submitButton(){
+    return RaisedButton(
+      onPressed: (){
+
+      },
+      color: Colors.blue,
+      child: Text('Login'),
+    );
   }
 }
